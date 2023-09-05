06a3e356b1c02267f6da00bc44b23ba7e873321e
gpdmadhushani <madhushani577@gmail.com>
2023-09-05 16:45:00 +0530

    first commit

---
+++ src/rentcar/layerd/RentcarLayerd.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rentcar.layerd;
+import rentcar.layerd.view.LoginView;
+import rentcar.layerd.view.RegistrationView;
+
+/**
+ *
+ * @author TOSHIBA
+ */
+public class RentcarLayerd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new LoginView().setVisible(true);
+    }
+    
+}
